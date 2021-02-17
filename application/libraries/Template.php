656b3a29b16bdd19f5086a2d92305ae4a35971f8
jawaldi348 <jawaldi416@gmail.com>
2021-02-17 18:38:48 +0700

    seting librari untuk layout template

---
+++ application/libraries/Template.php
@@ -0,0 +1,29 @@
+<?php
+class Template
+{
+    protected $_ci;
+    function __construct()
+    {
+        $this->_ci = &get_instance();
+    }
+    function auth($template, $data = null)
+    {
+        $data['content'] = $this->_ci->load->view($template, $data, true);
+        $this->_ci->parser->parse('auth/index', $data);
+    }
+    function dashboard($template, $data = null)
+    {
+        $data['content'] = $this->_ci->load->view($template, $data, true);
+        $this->_ci->parser->parse('layout/index', $data);
+    }
+    function modal_form($form, $data)
+    {
+        $data['body'] = $this->_ci->load->view($form, $data, true);
+        $this->_ci->load->view('layout/modal/modal_form', $data);
+    }
+    function modal_info($form, $data)
+    {
+        $data['body'] = $this->_ci->load->view($form, $data, true);
+        $this->_ci->load->view('layout/modal/modal_info', $data);
+    }
+}
