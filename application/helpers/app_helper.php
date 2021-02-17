7cae8abd4e709136b3c130d048e2e1db69170415
jawaldi348 <jawaldi416@gmail.com>
2021-02-17 22:41:01 +0700

    tambah function cek session dan session id_user

--- application/helpers/app_helper.php
+++ application/helpers/app_helper.php
@@ -23,3 +23,23 @@ if (!function_exists('faviconApp')) {
         return base_url() . 'assets/logo/favicon.ico';
     }
 }
+
+// cek session user yang login
+if (!function_exists('cek_user')) {
+    function cek_user()
+    {
+        $CI = &get_instance();
+        if ($CI->session->userdata('status_login') != 'sessPOS') :
+            redirect('logout');
+        endif;
+    }
+}
+
+// session kode user
+if (!function_exists('id_user')) {
+    function id_user()
+    {
+        $CI = &get_instance();
+        return $CI->session->userdata('kode');
+    }
+}
