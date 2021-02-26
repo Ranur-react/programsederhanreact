0190c7afa7b8186d5b3c0b4fdc9207c9c888a7a1
jawaldi348 <jawaldi416@gmail.com>
2021-02-26 16:31:51 +0700

    load library datatables,sweetalert2 dan rubah style css modal

--- application/views/layout/index.php
+++ application/views/layout/index.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="<?= assets() ?>bower_components/bootstrap/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?= assets() ?>bower_components/font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" href="<?= assets() ?>bower_components/Ionicons/css/ionicons.min.css">
+    <link rel="stylesheet" href="<?= assets() ?>bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
     <link rel="stylesheet" href="<?= assets() ?>dist/css/AdminLTE.min.css">
     <link rel="stylesheet" href="<?= assets() ?>dist/css/skins/_all-skins.min.css">
     <link rel="stylesheet" href="<?= assets() ?>bower_components/icomoon/styles.css">
@@ -21,13 +22,50 @@
         $.widget.bridge('uibutton', $.ui.button);
     </script>
     <script src="<?= assets() ?>bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="<?= assets() ?>bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
+    <script src="<?= assets() ?>bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
     <script src="<?= assets() ?>bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
     <script src="<?= assets() ?>bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
     <script src="<?= assets() ?>bower_components/fastclick/lib/fastclick.js"></script>
     <script src="<?= assets() ?>dist/js/adminlte.min.js"></script>
+    <script src="<?= assets() ?>dist/js/demo.js"></script>
+    <script src="<?= assets() ?>plugins/sweetalert2/sweetalert2.all.min.js"></script>
+    <script>
+        $(function() {
+            $('#data-tabel').DataTable({
+                ordering: false
+            })
+        });
+    </script>
     <style>
         .dropdown-menu>li>a {
             padding: 10px 20px;
+        }
+
+        .modal-content {
+            border-radius: 5px;
+        }
+
+        .modal-header {
+            background: #f3f4f5;
+            border-bottom: 1px solid #dcdfe1;
+            border-top-right-radius: .3rem;
+            border-top-left-radius: .3rem;
+        }
+
+        .modal-footer {
+            background: #f3f4f5;
+            border-top: 1px solid #dcdfe1;
+            border-bottom-right-radius: .3rem;
+            border-bottom-left-radius: .3rem;
+        }
+
+        label {
+            font-weight: 400;
+        }
+
+        .form-control {
+            border-radius: 3px;
         }
 
         @media (max-width: 767px) {
@@ -71,7 +109,7 @@
         </aside>
         <div class="content-wrapper">
             <section class="content-header">
-                <h1>{menu} <small>{small}</small></h1>
+                <h1>{title} <small>{small}</small></h1>
                 <ol class="breadcrumb">
                     <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
                     {links}
