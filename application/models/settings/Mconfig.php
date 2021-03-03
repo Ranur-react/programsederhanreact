e350c070c5fa009672622f213b70ad17b742f839
jawaldi348 <jawaldi416@gmail.com>
2021-03-03 18:19:21 +0700

    panggil beberapa konfigurasi dari database

---
+++ application/models/settings/Mconfig.php
@@ -0,0 +1,32 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Mconfig extends CI_Model
+{
+    public function nameApp()
+    {
+        return $this->db->where('nama_seting', 'nameapp')->get('settings')->row();
+    }
+    public function pathAssets()
+    {
+        return $this->db->where('nama_seting', 'pathassets')->get('settings')->row();
+    }
+    public function logoApp()
+    {
+        return $this->db->where('nama_seting', 'pathlogo')->get('settings')->row();
+    }
+    public function logoDashboard()
+    {
+        return $this->db->where('nama_seting', 'pathlogohome')->get('settings')->row();
+    }
+    public function faviconApp()
+    {
+        return $this->db->where('nama_seting', 'pathfavicon')->get('settings')->row();
+    }
+    public function noUserImage()
+    {
+        return $this->db->where('nama_seting', 'pathnouser')->get('settings')->row();
+    }
+}
+
+/* End of file Mconfig.php */
