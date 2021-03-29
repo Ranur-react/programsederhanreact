ca70c44f75df0166bbd06d00a1a74b9c54e438b9
jawaldi348 <jawaldi416@gmail.com>
2021-03-29 20:19:57 +0700

    tambah style animasi rotate

--- application/views/layout/index.php
+++ application/views/layout/index.php
@@ -44,6 +44,32 @@
     <script src="<?= assets() ?>js/app.js"></script>
     <script src="<?= assets() ?>js/common.js"></script>
     <script src="<?= assets() ?>js/currency.js"></script>
+    <style>
+        .animation-rotate {
+            -webkit-animation: spin .5s linear infinite;
+            -moz-animation: spin .5s linear infinite;
+            animation: spin .5s linear infinite;
+        }
+
+        @-moz-keyframes spin {
+            100% {
+                -moz-transform: rotate(360deg);
+            }
+        }
+
+        @-webkit-keyframes spin {
+            100% {
+                -webkit-transform: rotate(360deg);
+            }
+        }
+
+        @keyframes spin {
+            100% {
+                -webkit-transform: rotate(360deg);
+                transform: rotate(360deg);
+            }
+        }
+    </style>
 </head>
 
 <body class="hold-transition skin-blue">
