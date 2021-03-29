96d5cb7bebc5de737f2c193c00893ce991eca41d
jawaldi348 <jawaldi416@gmail.com>
2021-03-29 20:08:56 +0700

    tambah font awesome 5

--- application/views/layout/index.php
+++ application/views/layout/index.php
@@ -8,6 +8,7 @@
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <link rel="shortcut icon" href="<?= faviconApp() ?>">
     <link rel="stylesheet" href="<?= assets() ?>bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?= assets() ?>bower_components/font-awesome-5/css/all.css">
     <link rel="stylesheet" href="<?= assets() ?>bower_components/font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" href="<?= assets() ?>bower_components/source-sans/source-sans-pro.css">
     <link rel="stylesheet" href="<?= assets() ?>bower_components/Ionicons/css/ionicons.min.css">
