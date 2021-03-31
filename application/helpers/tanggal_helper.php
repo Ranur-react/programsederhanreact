c814976aba7c2c7742aa937b2fe6493004acf1e8
jawaldi348 <jawaldi416@gmail.com>
2021-03-31 22:34:32 +0700

    tambah helper tanggal

---
+++ application/helpers/tanggal_helper.php
@@ -0,0 +1,13 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+if (!function_exists('format_biasa')) {
+    function format_biasa($tgl)
+    {
+        $ubah = gmdate($tgl, time() + 60 * 60 * 8);
+        $pecah = explode("-", $ubah);
+        $tanggal = $pecah[2];
+        $bulan = $pecah[1];
+        $tahun = $pecah[0];
+        return $tanggal . '-' . $bulan . '-' . $tahun;
+    }
+}
