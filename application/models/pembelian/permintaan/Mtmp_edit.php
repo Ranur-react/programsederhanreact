c1319a80fc55a21f804e0bc5bcfa4388c2142058
jawaldi348 <jawaldi416@gmail.com>
2021-04-01 14:01:29 +0700

    update total permintaan

--- application/models/pembelian/permintaan/Mtmp_edit.php
+++ application/models/pembelian/permintaan/Mtmp_edit.php
@@ -16,6 +16,14 @@ class Mtmp_edit extends CI_Model
         $query = $this->db->select('IFNULL(SUM(harga_detail*jumlah_detail),0) AS total')->where('permintaan_detail', $kode)->get('permintaan_detail')->row();
         return $query->total;
     }
+    public function update_total($kode)
+    {
+        $total = $this->get_total($kode);
+        $data = array(
+            'total_permintaan' => $total
+        );
+        return $this->db->where('id_permintaan', $kode)->update('permintaan', $data);
+    }
 }
 
 /* End of file Mtmp_edit.php */
