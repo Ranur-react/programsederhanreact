47cd5f4f7966e2b52d2be775cd44163e0ca1c0d3
jawaldi348 <jawaldi416@gmail.com>
2021-04-15 12:57:40 +0700

    update query model

--- application/models/pembelian/penerimaan/Mtmp_edit.php
+++ application/models/pembelian/penerimaan/Mtmp_edit.php
@@ -5,7 +5,8 @@ class Mtmp_edit extends CI_Model
 {
     public function tampil_data($kode)
     {
-        return $this->db->from('penerimaan_detail')
+        return $this->db->select('*,penerimaan_detail.id_detail AS id_detail_terima,penerimaan_detail.harga_detail AS harga_terima,penerimaan_detail.jumlah_detail AS jumlah_terima')
+            ->from('penerimaan_detail')
             ->join('permintaan_detail', 'minta_detail=permintaan_detail.id_detail')
             ->join('barang_satuan', 'barang_detail=id_brg_satuan')
             ->join('barang', 'barang_brg_satuan=id_barang')
--- application/views/pembelian/penerimaan/tmp_edit/data.php
+++ application/views/pembelian/penerimaan/tmp_edit/data.php
@@ -4,14 +4,14 @@ else :
     $no = 1;
     $total = 0;
     foreach ($data as $d) {
-        $total = $total + ($d['harga_detail'] * $d['jumlah_detail']);
+        $total = $total + ($d['harga_terima'] * $d['jumlah_terima']);
 ?>
         <tr>
             <td class="text-center"><?= $no . '.' ?></td>
             <td><?= $d['nama_barang'] ?></td>
-            <td><?= akuntansi($d['harga_detail']) ?></td>
-            <td class="text-right"><?= rupiah($d['jumlah_detail']) . ' ' . $d['singkatan_satuan'] ?></td>
-            <td><?= akuntansi($d['harga_detail'] * $d['jumlah_detail']) ?></td>
+            <td><?= akuntansi($d['harga_terima']) ?></td>
+            <td class="text-right"><?= rupiah($d['jumlah_terima']) . ' ' . $d['singkatan_satuan'] ?></td>
+            <td><?= akuntansi($d['harga_terima'] * $d['jumlah_terima']) ?></td>
             <td class="text-center">
                 <a href="javascript:void(0)"><i class="icon-pencil7 text-green" title="Edit Data"></i></a>
                 <a href="javascript:void(0)"><i class="icon-trash text-red" title="Hapus Data"></i></a>
