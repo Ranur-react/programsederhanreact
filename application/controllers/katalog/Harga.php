631fa8a40682d592e15470851e35c852f90fee9a
jawaldi348 <jawaldi416@gmail.com>
2021-05-12 19:08:32 +0700

    ganti nama variabel

--- application/controllers/katalog/Harga.php
+++ application/controllers/katalog/Harga.php
@@ -23,30 +23,30 @@ class Harga extends CI_Controller
     }
     public function data()
     {
-        $list = $this->Mharga->fetch_all();
+        $results = $this->Mharga->fetch_all();
         $data = array();
         $no = $_GET['start'];
-        foreach ($list as $value) {
+        foreach ($results as $result) {
             $detail = '<a href="#"><i class="icon-eye8 text-black" title="Detail"></i></a>';
             $histori = '<a href="#"><i class="icon-history text-green" title="Riwayat Harga"></i></a>';
 
             $no++;
-            $row = array();
-            $row[] = $no . '.';
-            $row[] = $value->nama_barang;
-            $row[] = '';
-            $row[] = '';
-            $row[] = status_span($value->status_barang, 'aktif');
-            $row[] = $detail . '&nbsp;' . $histori;
-            $data[] = $row;
+            $rows = array();
+            $rows[] = $no . '.';
+            $rows[] = $result->nama_barang;
+            $rows[] = '';
+            $rows[] = '';
+            $rows[] = status_span($result->status_barang, 'aktif');
+            $rows[] = $detail . '&nbsp;' . $histori;
+            $data[] = $rows;
         }
-        $output = array(
+        $json = array(
             "draw" => $_GET['draw'],
             "recordsTotal" => $this->Mharga->count_all(),
             "recordsFiltered" => $this->Mharga->count_filtered(),
             "data" => $data,
         );
-        echo json_encode($output);
+        echo json_encode($json);
     }
 }
 
