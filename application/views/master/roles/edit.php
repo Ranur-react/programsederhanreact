55d2be0baae6082c6cb7b3ed67dfadd69eb3d4c1
jawaldi348 <jawaldi416@gmail.com>
2021-06-15 12:30:29 +0700

    hilangkan jenis hak akses

--- application/views/master/roles/edit.php
+++ application/views/master/roles/edit.php
@@ -3,11 +3,3 @@
     <label>Hak Akses</label>
     <input type="text" name="nama" id="nama" class="form-control" value="<?= $data['nama_role'] ?>">
 </div>
-<div class="form-group">
-    <label>Jenis Hak Akses</label>
-    <select class="form-control" name="jenis" id="jenis">
-        <option value="">--- Pilih ---</option>
-        <option value="1" <?= $data['jenis_role'] == '1' ? 'selected' : null ?>>Back Office</option>
-        <option value="2" <?= $data['jenis_role'] == '2' ? 'selected' : null ?>>Gudang</option>
-    </select>
-</div>
