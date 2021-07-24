ba8a0f7314389e29945ad5facb564f57447c230b
jawaldi348 <jawaldi416@gmail.com>
2021-07-24 12:29:25 +0700

    tambah helper baru untuk handle auth dan rememberme

--- application/helpers/app_helper.php
+++ application/helpers/app_helper.php
@@ -23,6 +23,7 @@ if (!function_exists('logoApp')) {
         return $image;
     }
 }
+
 // Logo Aplikasi
 if (!function_exists('logoDashboard')) {
     function logoDashboard()
@@ -44,69 +45,5 @@ if (!function_exists('faviconApp')) {
         $data = $CI->Mconfig->faviconApp();
         $image = $data->value_seting;
         return $image;
-    }
-}
-
-// cek session user yang login
-if (!function_exists('cek_user')) {
-    function cek_user()
-    {
-        $CI = &get_instance();
-        if ($CI->session->userdata('status_login') != 'sessDashboard') :
-            redirect('logout');
-        endif;
-    }
-}
-
-// session kode user
-if (!function_exists('id_user')) {
-    function id_user()
-    {
-        $CI = &get_instance();
-        return $CI->session->userdata('kode');
-    }
-}
-
-// nama user yang login
-if (!function_exists('user_profile')) {
-    function user_profile()
-    {
-        $CI = &get_instance();
-        $row = $CI->db->where('id_user', id_user())->get('users')->row_array();
-        return $row['nama_user'];
-    }
-}
-
-// avatar user
-if (!function_exists('user_photo')) {
-    function user_photo()
-    {
-        $CI = &get_instance();
-        $row = $CI->db->where('id_user', id_user())->get('users')->row_array();
-        if ($row['avatar_user'] != null) {
-            return assets() . $row['avatar_user'];
-        } else {
-            $CI->load->model('settings/Mconfig');
-            $data = $CI->Mconfig->noUserImage();
-            $image = $data->value_seting;
-            return $image;
-        }
-    }
-}
-
-// role user
-if (!function_exists('role_user')) {
-    function role_user()
-    {
-        $CI = &get_instance();
-        $row = $CI->db->where('id_user', id_user())->get('users')->row_array();
-        if ($row['jenis_user'] == 1) :
-            $result = $CI->db->from('role')
-                ->join('user_office', 'id_role=role_level')
-                ->where('user_level', id_user())
-                ->get()->row_array();
-            $role = $result['nama_role'];
-        endif;
-        return $role;
     }
 }
