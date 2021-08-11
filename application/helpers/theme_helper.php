573edf8b7dc1786a12086d02067b2a7e24589809
danirahmat354 <danirahmat019@gmail.com>
2021-08-12 06:34:40 +0700

    assets css

--- application/helpers/theme_helper.php
+++ application/helpers/theme_helper.php
@@ -11,6 +11,14 @@ if (!function_exists('assets')) {
     }
 }
 
+if (!function_exists('assets_css')) {
+    function assets_css()
+    {
+        $link = base_url() . 'src/css/';
+        return $link;
+    }
+}
+
 if (!function_exists('assets_js')) {
     function assets_js()
     {
