d45c45799f18af2a448bd266588aec90864c08ab
jawaldi348 <jawaldi416@gmail.com>
2021-08-25 19:15:40 +0700

    pemisah mata uang

--- src/js/common.js
+++ src/js/common.js
@@ -4,6 +4,19 @@ function resetToken(token) {
     $("#csrf_token,.csrf_token").val(token);
 }
 
+// Pemisah Mata Uang
+function addCommas(nStr) {
+    nStr += '';
+    var x = nStr.split('.');
+    var x1 = x[0];
+    var x2 = x.length > 1 ? '.' + x[1] : '';
+    var rgx = /(\d+)(\d{3})/;
+    while (rgx.test(x1)) {
+        x1 = x1.replace(rgx, '$1' + '.' + '$2');
+    }
+    return x1 + x2;
+}
+
 // Autocomplete */
 (function ($) {
     $.fn.autocomplete = function (option) {
