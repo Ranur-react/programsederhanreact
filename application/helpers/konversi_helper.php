5516325c5e51d9969cb24b6d347e3dcdc440a8c1
jawaldi348 <jawaldi416@gmail.com>
2021-10-23 12:23:27 +0700

    konversi nilai satuan dari terkecil ke terbesar

--- application/helpers/konversi_helper.php
+++ application/helpers/konversi_helper.php
@@ -7,7 +7,6 @@ if (!function_exists('konversi_jumlah_satuan')) {
         $satuan_terbesar = $CI->db->from('satuan_konversi')
             ->where('idsatuan_terbesar', $idsatuan);
         if ($satuan_terbesar->count_all_results() > 0) :
-            // Dikali
             $data_terbesar = $CI->db->select('ks1.idsatuan_terbesar AS idsatuan_besar,ks1.idsatuan_terkecil AS idsatuan_kecil,s2.nama_satuan AS satuan_terkecil,nilai_konversi')
                 ->from('satuan_konversi AS ks1')
                 ->join('satuan AS s1', 'ks1.idsatuan_terbesar=s1.id_satuan')
@@ -24,7 +23,6 @@ if (!function_exists('konversi_jumlah_satuan')) {
         $satuan_terkecil = $CI->db->from('satuan_konversi')
             ->where('idsatuan_terkecil', $idsatuan);
         if ($satuan_terkecil->count_all_results() > 0) :
-            // Dibagi
             $data_terkecil = $CI->db->select('ks1.idsatuan_terbesar AS idsatuan_besar,ks1.idsatuan_terkecil AS idsatuan_kecil,s1.nama_satuan AS satuan_terbesar')
                 ->from('satuan_konversi AS ks1')
                 ->join('satuan AS s1', 'ks1.idsatuan_terbesar=s1.id_satuan')
@@ -35,6 +33,34 @@ if (!function_exists('konversi_jumlah_satuan')) {
                 'idsatuan_besar' => (int)$data_terkecil['idsatuan_besar'],
                 'idsatuan_kecil' => (int)$data_terkecil['idsatuan_kecil'],
                 'jumlah' => (int)$jumlah
+            ];
+        endif;
+        return $data;
+    }
+}
+
+if (!function_exists('konversi_nilai_satuan')) {
+    function konversi_nilai_satuan($idsatuan, $jumlah)
+    {
+        $CI = &get_instance();
+        $satuan_terbesar = $CI->db->from('satuan_konversi')
+            ->where('idsatuan_terbesar', $idsatuan);
+        if ($satuan_terbesar->count_all_results() > 0) :
+            // Dibagi
+            $data_terbesar = $CI->db->select('ks1.idsatuan_terbesar AS idsatuan_besar,ks1.idsatuan_terkecil AS idsatuan_kecil,s2.nama_satuan AS satuan_terkecil,nilai_konversi')
+                ->from('satuan_konversi AS ks1')
+                ->join('satuan AS s1', 'ks1.idsatuan_terbesar=s1.id_satuan')
+                ->join('satuan AS s2', 'ks1.idsatuan_terkecil=s2.id_satuan')
+                ->where('ks1.idsatuan_terbesar', $idsatuan)
+                ->get()->row_array();
+            $proses = $jumlah / $data_terbesar['nilai_konversi'];
+            $sisa_bagi = $jumlah % $data_terbesar['nilai_konversi'];
+            $hasil = ($jumlah - $sisa_bagi) / $data_terbesar['nilai_konversi'];
+            $value = $sisa_bagi == 0 ? $proses : $hasil . ',' . $sisa_bagi;
+            $data = [
+                'idsatuan_besar' => (int)$data_terbesar['idsatuan_besar'],
+                'idsatuan_kecil' => (int)$data_terbesar['idsatuan_kecil'],
+                'jumlah' => $value
             ];
         endif;
         return $data;
